791ac6f3e607abcb9ea4ffa5e867eea7be1f0a57
Juan Pablo <jurado.triana98@gmail.com>
2024-03-19 00:43:08 -0600

    print HP, Str, int, of classes

--- main.cpp
+++ main.cpp
@@ -10,7 +10,19 @@ int main() {
     std::cout << "Warrior\n"
         << "-MaxHP: " << warr1.getMaxHP() << '\n'
         << "-Strength: " << warr1.getStrength() << '\n'
-        << "Intellect: " << warr1.getIntellect() << '\n';
+        << "-Intellect: " << warr1.getIntellect() << '\n';
+    std::cout << "Warrior\n"
+        << "-MaxHP: " << wiz1.getMaxHP() << '\n'
+        << "-Strength: " << wiz1.getStrength() << '\n'
+        << "-Intellect: " << wiz1.getIntellect() << '\n';
+    std::cout << "Warrior\n"
+        << "-MaxHP: " << cler1.getMaxHP() << '\n'
+        << "-Strength: " << cler1.getStrength() << '\n'
+        << "-Intellect: " << cler1.getIntellect() << '\n';
+    std::cout << "Warrior\n"
+        << "-MaxHP: " << rog1.getMaxHP() << '\n'
+        << "-Strength: " << rog1.getStrength() << '\n'
+        << "-Intellect: " << rog1.getIntellect() << '\n';
 
     return 0;
 }
